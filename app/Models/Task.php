8bbef0c0262da53b4e5065dbd8d8b7359b0c3129
sampleTK <sample@gmail.com>
2025-05-16 00:01:26 +0900

    use 読み込み順整理

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 
 class Task extends Model
 {
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     if (Auth::check()) {
